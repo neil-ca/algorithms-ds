1d4a858b55476b6e12879045dbc2887bd68c9190
ulises <ulisescamps9@gmail.com>
2020-10-03 23:57:56 -0500

    last node

--- datastructures/linked_list.go
+++ datastructures/linked_list.go
@@ -33,6 +33,30 @@ func (linkedList *LinkedList) IterateList() {
 		fmt.Println(node.property)
 	}
 }
+
+// LastNode method return the last Node
+func (linkedList *LinkedList) LastNode() *Node {
+	var node *Node
+	var lastNode *Node
+	for node = linkedList.headNode; node != nil; node = node.nextNode {
+		if node.nextNode == nil {
+			lastNode = node
+		}
+	}
+	return lastNode
+}
+
+// AddToEnd method adds the node with property to the end
+func (linkedList *LinkedList) AddToEnd(property int) {
+	var node = &Node{}
+	node.property = property
+	node.nextNode = nil
+	var lastNode *Node
+	lastNode = linkedList.LastNode()
+	if lastNode != nil {
+		lastNode.nextNode = node
+	}
+}
 func main() {
 	var linkedList LinkedList
 	linkedList = LinkedList{}
