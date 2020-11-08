1361630c894a03a4ee62aec73074ae3e27687c00
ulises <ulisescamps9@gmail.com>
2020-11-07 19:48:54 -0600

    complementing with C

---
+++ c/aritmeti.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int dato1, dato2, resultado;
+	dato1 = 5000000;
+	dato2 = 2;
+
+	// Suma
+	resultado = dato1 + dato2;
+	printf("%d + %d = %d\n", dato1, dato2, resultado);
+
+	// Resta
+	resultado = dato1 - dato2;
+	printf("%d - %d = %d\n", dato1, dato2, resultado);
+
+	// Producto
+	resultado = dato1 * dato2;
+	printf("%d * %d = %d\n", dato1, dato2, resultado);
+
+	// Cociente
+	resultado = dato1 / dato2;
+	printf("%d / %d = %d\n", dato1, dato2, resultado);
+ 	
+	// Resto
+	resultado = dato1 % dato2;
+	printf("%d % %d = %d\n", dato1, dato2, resultado);
+}	
