656808ea2bacb7f2a083da8f872cb14f925f1d86
ulises <ulisescamps9@gmail.com>
2020-12-05 23:33:17 -0600

    a trivial swing user interface

---
+++ java/Gui.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Gui {
+	public static void main (String[] args) {
+		JFrame frame = new JFrame("GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+}
