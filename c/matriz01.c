c9d5b12b9bf7eb9ad617d80effafef374c662235
ulises <ulisescamps9@gmail.com>
2020-12-17 23:30:44 -0600

    matriz in c

---
+++ c/matriz01.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define N_ELEMENTOS 10
+
+int main() {
+	int m[N_ELEMENTOS];
+	int i = 0;
+
+	printf("Introduce los valores de la matriz\n");
+
+	// entrada de datos
+	for (i = 0; i < N_ELEMENTOS; i++)
+	{
+	  printf("m[%d] = ", i);
+ 	  scanf("%d", &m[i]);
+	}
+
+	// salida
+	for (i = 0; i < N_ELEMENTOS; i++)
+		printf("%d\n", m[i]);
+}
