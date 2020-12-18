5db3b83f98f4ceedb31e2de3f62afa46dd8c7ff5
ulises <ulisescamps9@gmail.com>
2020-12-18 02:24:48 -0600

    structs with pointers

---
+++ c/Books.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <stdio.h>
+
+struct Books {
+    char title[50];
+    char author[50];
+    char subject[100];
+    int book_id;
+};
+
+void printBook(struct Books *book);
+int main() {
+
+    struct Books Book1;
+    struct Books Book2;
+
+    strcpy(Book1.title, "C programming");
+    strcpy(Book1.author, "Nuha Ali");
+    strcpy(Book1.subject, "C structs");
+    Book1.book_id = 968498;
+
+    strcpy( Book2.title, "Telecom Billing");
+    strcpy( Book2.author, "Zara Ali");
+    strcpy( Book2.subject, "Telecom Billing Tutorial");
+    Book2.book_id = 6495700;
+
+    printBook(&Book1);
+    printBook(&Book2);
+    return 0; 
+}
+void printBook(struct Books *book) {
+      printf( "Book title : %s\n", book->title);
+   printf( "Book author : %s\n", book->author);
+   printf( "Book subject : %s\n", book->subject);
+   printf( "Book book_id : %d\n", book->book_id);
+}
