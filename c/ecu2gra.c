4dfa578a91e161f0a8ce8a8603a71c1a1978e7f3
ulises <ulisescamps9@gmail.com>
2021-01-14 23:59:35 -0600

    exuacion de 2 grado
    Signed-off-by: ulises <ulisescamps9@gmail.com>

---
+++ c/ecu2gra.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+void main() {
+    double a, b, c; // coeficientes de la ecuacion
+    double d; // discriminante
+    double re, im; // parte real e imaginaria de la raiz
+
+    printf("Coeficientes a, b y c de la ecuacion: ");
+    scanf("%1f %1f %1f", &a &b &c);
+}
