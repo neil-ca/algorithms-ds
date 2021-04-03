7cd938d5b973f8d9a7f3ef9ac30f8b732acd362f
ulises <ulisescamps9@gmail.com>
2021-04-02 23:57:28 -0600

    using range function in python

---
+++ python/range.py
@@ -0,0 +1,10 @@
+# Range function can be used to easily generate a large list of numbers. It is used to 
+# autopopulate sequences of numbers in a list.
+# range(start, stop, step)
+x = range(6)
+for n in x:
+    print(n)
+
+oddNum = range(3, 29, 2)
+for n in oddNum:
+    print(n)
