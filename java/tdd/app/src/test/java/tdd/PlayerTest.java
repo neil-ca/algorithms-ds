f31b52e9a3a7d293c82b1f9313dfdc0387edfcee
ulises <ulisescamps9@gmail.com>
2021-04-17 11:36:43 -0500

    adding more tests

---
+++ java/tdd/app/src/test/java/tdd/PlayerTest.java
@@ -0,0 +1,11 @@
+package tdd;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PlayerTest {
+    @Test public void loses_when_dice_number_is_low() {
+        assertEquals(false, true);
+    }
+}
