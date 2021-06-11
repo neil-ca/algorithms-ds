a2e27c762505ebf400127cd32b4fc4ad92b16b75
ulises <ulisescamps9@gmail.com>
2021-06-10 22:11:58 -0500

    BubbleSort in go

--- golang/algorithms/BubbleSort.go
+++ golang/algorithms/BubbleSort.go
@@ -1,11 +1,22 @@
 package main
-
-import (
-    "fmt"
-)
-
+import "fmt"
 func main() {
-    var scores = []int{90,70,50,80,60,85}
+    // index starts from 0
+    var scores = []int{90, 70, 50, 80, 60, 85}
     var length = len(scores)
+    sort(scores, length)
+    for i := 0; i < length; i++ {
+        fmt.Printf("%d,", scores[i])
+    }
 }
-
+func sort(arrays []int, length int) {
+    for i := 0; i < length-1; i++ {
+        for j := 0; j < length-i-1; j++ {
+            if arrays[j] > arrays[j+1] { //swap
+                var flag = arrays[j]
+                arrays[j] = arrays[j+1]
+                arrays[j+1] = flag
+            }
+        }
+    }
+}
